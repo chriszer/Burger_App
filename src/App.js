87c1d578ce63d9f9c9a5ff9f18dd51b27e38b748
chriszer tamayo <chriszer.myth@gmail.com>
2020-04-04 00:44:48 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css'
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="App">
+       <Layout>
+         <BurgerBuilder></BurgerBuilder>
+       </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
